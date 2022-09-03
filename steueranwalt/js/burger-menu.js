09ce6f20e620771a361e1e8b051c36afde46218a
OleksandrRyzhov <oleksandr.ryzhov@nure.ua>
2022-09-03 12:43:03 +0300

    some fix

--- steueranwalt/js/burger-menu.js
+++ steueranwalt/js/burger-menu.js
@@ -4,7 +4,7 @@ const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle("._lock");
+    document.body.classList.toggle("_lock");
     iconMenu.classList.toggle("_active");
     menuBody.classList.toggle("_active");
   });
