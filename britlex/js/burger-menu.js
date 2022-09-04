6672718d34c33e1d1040aac6964973a4c21a0d9e
OleksandrRyzhov <oleksandr.ryzhov@nure.ua>
2022-09-04 22:15:39 +0300

    added adaptive for section skills and some change burger menu

--- britlex/js/burger-menu.js
+++ britlex/js/burger-menu.js
@@ -2,12 +2,10 @@
 
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
-const headerTopBtn = document.querySelector(".header__top-btn");
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
     document.body.classList.toggle("_lock");
     iconMenu.classList.toggle("_active");
     menuBody.classList.toggle("_active");
-    headerTopBtn.classList.toggle("_active");
   });
 }
